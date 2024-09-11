1579dde0d0c3f8ae607c4ad4438e6b4fa5eec6cc
voquynhnga <voquynhnga75@gmail.com>
2024-09-11 23:57:10 +0700

    Buoi1_1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,27 @@
+function createMeeting() {
+    let newMeetingCode = Math.random().toString(36).substring(2, 8);
+    alert("Tạo cuộc họp mới với mã: " + newMeetingCode);
+    window.location.href = `meeting.html?code=${newMeetingCode}`;
+}
 
-document.getElementById("NewClassRoom").addEventListener("click", function() {
-        alert("Tạo cuộc họp mới thành công");
-    });
+function joinMeeting() {
+    let meetingCode = document.getElementById("meetingCode").value;
+    if (meetingCode) {
+        window.location.href = `meeting.html?code=${meetingCode}`;
+    } else {
+        alert("Vui lòng nhập mã cuộc họp!");
+    }
+}
+
+function enterCode(){
+    const meetingCodeInput = document.getElementById('meetingCode');
+    const joinButton = document.getElementById('joinButton');
+
+    if (meetingCodeInput.value.trim() !== "" && meetingCodeInput.value !== "Nhập mã cuộc họp") {
+        joinButton.disabled = false; 
+        joinButton.classList.add('active'); // Thêm class active để có màu xanh
+    } else {
+        joinButton.disabled = true;  // Vô hiệu hóa nút
+        joinButton.classList.remove('active'); // Loại bỏ class active
+    }
+}
